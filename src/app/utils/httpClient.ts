1b6789f94ed077e4ecd57b53930438afdc20427b
eliasgermoliato <eliasgermoliato@hotmail.com>
2023-08-27 14:02:14 -0300

    feat: creating feature to define the bearer token in each request

--- src/app/utils/httpClient.ts
+++ src/app/utils/httpClient.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { localStorageKeys } from "../config/localStorageKeys";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN.key);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${JSON.parse(accessToken)}`;
+  }
+
+  return config;
 });
