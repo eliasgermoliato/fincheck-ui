afe420da7e1d58c4452219c0e55d31d7b6faf997
eliasgermoliato <eliasgermoliato@hotmail.com>
2023-08-28 19:48:19 -0300

    feat: adding responsiveness to the layout of private pages

--- src/view/layouts/PrivatePageLayout.tsx
+++ src/view/layouts/PrivatePageLayout.tsx
@@ -4,7 +4,7 @@ import { UserMenu } from "../components/UserMenu";
 
 export function PrivatePageLayout() {
   return (
-    <div className="w-full h-full p-8 pt-6 flex flex-col gap-4">
+    <div className="w-full h-full p-4 md:p-8 md:pt-6 flex flex-col gap-4">
       <header className="h-12 flex items-center justify-between">
         <Logo className="h-6 text-teal-900" />
         <UserMenu />
