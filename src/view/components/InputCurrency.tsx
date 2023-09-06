c49a5d1c7fbd20bf5456552e26d9c287b21f56d2
eliasgermoliato <eliasgermoliato@hotmail.com>
2023-09-05 23:51:49 -0300

    style: setting the width and defaultValue of the InputCurrency component

--- src/view/components/InputCurrency.tsx
+++ src/view/components/InputCurrency.tsx
@@ -3,9 +3,10 @@ import { NumericFormat } from "react-number-format";
 export function InputCurrency() {
   return (
     <NumericFormat
-      className="text-[32px] font-bold tracking-customTighter outline-none text-gray-900"
+      className="w-full text-[32px] font-bold tracking-customTighter outline-none text-gray-900"
       thousandSeparator="."
       decimalSeparator=","
+      defaultValue="0"
     />
   );
 }
